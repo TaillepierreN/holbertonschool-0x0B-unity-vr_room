7eb0201bf9e4561fa55d9f9fb724ac3c48818922
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2025-05-19 16:56:46 +0200

    added interaction / gameplay/ audio

--- Assets/Scripts/DoorHandle.cs
+++ Assets/Scripts/DoorHandle.cs
@@ -1,14 +1,54 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorHandle : MonoBehaviour
 {
-    public Animator doorAnimator;
+    public Animator DoorAnimator;
+    public GameObject DoorTextPanel;
+    private bool Unlocked = false;
     private string boolParameterName = "character_nearby";
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip[] _audioClips;
     public void ToggleDoor()
     {
-        if (doorAnimator == null) return;
-
-        bool currentValue = doorAnimator.GetBool(boolParameterName);
-        doorAnimator.SetBool(boolParameterName, !currentValue);
+        if (DoorAnimator == null) return;
+        if (Unlocked)
+        {
+            bool currentValue = DoorAnimator.GetBool(boolParameterName);
+            if (currentValue)
+            {
+                _audioSource.PlayOneShot(_audioClips[1]);
+            }
+            else
+            {
+                _audioSource.PlayOneShot(_audioClips[2]);
+            }
+            DoorAnimator.SetBool(boolParameterName, !currentValue);
+        }
+        else
+        {
+            _audioSource.PlayOneShot(_audioClips[0]);
+            StartCoroutine(ShowDoorTextPanel());
+        }
     }
+
+    public void UnlockDoor()
+    {
+        Unlocked = true;
+    }
+
+    private IEnumerator ShowDoorTextPanel()
+    {
+        if (DoorTextPanel != null)
+        {
+            DoorTextPanel.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            DoorTextPanel.SetActive(false);
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+
 }
